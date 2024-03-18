1a852c8a1fc057857778922c3db2f3b2bad7985e
SriyansuDash <sriyansudash@outlook.com>
2024-03-18 10:44:14 +0530

    1 . Javascrpit codes are added 2. All files are updated \q /q \q

---
+++ neoWebDev/Javascript/Operators.js
@@ -0,0 +1,93 @@
+// Arithmatic Operation in Javascript
+
+let var1 = 12;
+let var2 = 6;
+
+// Arithmatic Addition
+console.log("var1 + var2 =",var1+var2 );
+// Output: "var1 + var2 ="  18
+
+// Arithmatic Subtraction
+console.log("var1 - var2 =", var1-var2);
+// Output : "var1 - var2 =" 6
+
+// Arithmatic Multiplication
+console.log("var1 * var2 =", var1*var2);
+//Output : :var1 *var2 =" 72
+
+// Arithmatatic Division
+console.log("var1 / var2 =", var1/ var2);
+// Output :"var1 / var2 =" 2
+
+let a = 234;
+let b = 10;
+
+//Reminder
+console.log("a % b =" ,a%b);
+// Output : "a % b ="4
+
+let c = 2;
+let d = 3;
+// Power Operation
+console.log("c ** d", c**d) ;
+// Output : "c ** d = 8
+console.log("c ** c", c**c);
+// Output : "c ** c = 4 
+
+console.log("Unary Operator..........");
+// Increment Operator
+console.log("var1 = ",var1);
+var1++;
+console.log("Incremented var1 value =", var1);
+// Output = 13
+// Decrement Operator
+console.log("var2 =",var2);
+var2--;
+console.log("Decremented var2 value = ",var2);
+// Output = 5
+
+//Pre Increment Operator
+console.log("var1 =", var1);
+++var1;
+console.log("Preincremented var1 value =", var1);
+// output = 14
+
+//Predecrement Operator
+console.log("var2 =", var2);
+--var2;
+console.log("Predecremente var",var2);
+// Output = 4
+
+console.log("Assignment Operator...........");
+// Addtional Assignment Operator
+console.log("var1 += var2 :", var1+=var2);
+// Output = 18 \\ var1 +=var2 means var1=var1+var2 VAR1 value changes to new value
+
+// Substraction Assignment Operator
+console.log("var1 -= var2 :", var1 -=var2);
+// Output = 14
+
+// Multiplication Assignment Operator
+console.log("var1 *=var2 :",var1*=var2);
+// Output = 56
+
+//Substraction Assignment Operator
+console.log("var1 /= var2 :", var1/=var2);
+//Output = 14
+
+// Modulous Assignment Operator 
+console.log("var1 %= var2",var1 %= var2);
+// Output = 2
+
+// Reminder Assignment Operator 
+console.log("var1 %= var2: " , var1%var2 ); 
+// Output = 2
+console.log("var1 :",var1);
+console.log("var2 :",var2);
+//Exponential Arithmetic Operator
+console.log("var1 **= var2 : ", var1 **= var2);
+// Output : 169025
+
+console.log("Comparision Operator .......")
+// Equal operator
+console.log("var1 == var2 :", var1 == var2);
---
+++ neoWebDev/Javascript/script.js
@@ -0,0 +1,44 @@
+console.log("Hello World");
+name = "Sriyansu";
+console.log(name);
+age = 18;
+console.log("My age is "+age);
+x = null;
+y = undefined;
+console.log(x +" " + y);
+price = 99.99;
+console.log(price);
+isBoolean = true;
+console.log(isBoolean);
+isBoolean2 = false;
+console.log(isBoolean2);
+
+let number = 12;
+number = 344
+console.log(number);
+const neo = 23;
+console.log(neo);
+
+const student = {
+    Fullname : "Sriyansu dash",
+    age : 18,
+    reg_no : 2241004190,
+    cgpa : 7.2
+};
+console.log(student);
+ console.log("Name = "+student.name);
+ console.log("Age = "+student.age);
+ console.log("Registration Number = "+ student.reg_no);
+ console.log("CGPA = "+student.cgpa);
+
+ student.age = student.age+1
+ console.log(student.age+1);
+
+ console.log("Print new function");
+ const insta={
+    name :" Sriyansu Dash",
+    username :"@sriyansu_dash",
+    followers : 143,
+    followings : 134
+ };
+ console.log(insta);
